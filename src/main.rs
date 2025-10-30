97dc998075d3fce44161fee034b8da0d5ab7f788
TidosDK <git@msnn.dev>
2025-10-30 14:09:56 +0100

    Working version

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,91 @@
-// TODO: Handle panics instead of using unwrap.
-
-use std::fs::{self, File};
-use std::path::{Path, PathBuf};
+use std::fs::{self, File, remove_dir_all};
+use std::io;
+use std::path::{Component, Path, PathBuf};
 
 use chrono::Local;
 use tar::Builder;
 
 fn main() {
-    let backup_folder: &str = "laptop-backup";
+    let backup_folder_path: &str = "laptop-backup";
 
-    backup_files_from("/home/matty/.ssh/", backup_folder);
+    let paths: Vec<&str> = vec![
+        "/home/matty/.ssh/",
+        "/home/matty/.kube/",
+        "/home/matty/Downloads/test/",
+    ];
 
-    zip_files_in_folder(backup_folder);
+    for path in &paths {
+        if let Err(err) = backup_files_from(path, &backup_folder_path) {
+            eprintln!("{:?}", err);
+        }
+    }
+
+    zip_files_in_folder(backup_folder_path);
 }
 
-pub fn backup_files_from(folder_path: &str, backup_folder: &str) {
-    let folder_path: PathBuf = PathBuf::from(folder_path);
+pub fn backup_files_from<T: AsRef<Path>>(source_path: T, backup_folder_path: T) -> io::Result<()> {
+    let source_path: PathBuf = PathBuf::from(source_path.as_ref());
+    let backup_folder_path: PathBuf = PathBuf::from(backup_folder_path.as_ref());
 
-    if let Ok(_) = fs::create_dir_all(backup_folder) {
-        println!("Created folder {:?}", backup_folder);
+    if !source_path.is_absolute() {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!("source path '{}' is not absolute", source_path.display()),
+        ));
     }
 
-    if !Path::new(backup_folder).exists() {
-        eprint!("The folder {:?} could not be created", backup_folder);
-        return;
+    if !source_path.is_dir() {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!("source path '{}' is not a directory", source_path.display()),
+        ));
     }
 
-    if folder_path.is_dir() {
-        if let Ok(entries) = fs::read_dir(&folder_path) {
-            for entry_result in entries {
-                if let Ok(entry) = entry_result {
-                    let path_to_file: PathBuf = entry.path();
-                    copy_file_from(path_to_file, backup_folder);
-                } else if let Err(e) = entry_result {
-                    eprintln!("Error reading entry: {}", e);
-                }
-            }
-        } else {
-            eprintln!("Failed to read directory {:?}", folder_path);
-        }
-    } else {
-        eprint!("The folder, {} is not a folder", folder_path.display());
+    let relative_source_path: PathBuf = PathBuf::from(
+        source_path
+            .strip_prefix(Component::RootDir)
+            .unwrap_or(&source_path), // unwrap_or returns the default value if the strip_prefix was not able to remove any RootDir component.
+    );
+
+    let full_backup_folder_path: PathBuf = backup_folder_path.join(relative_source_path);
+
+    fs::create_dir_all(&full_backup_folder_path)?;
+
+    for entry in fs::read_dir(&source_path)? {
+        let file_path = entry?.path();
+        copy_file_from(file_path, &full_backup_folder_path);
     }
+
+    return Ok(());
 }
 
 // TODO: It only copies files, not folders. Look at the "WalkDir" crate.
-fn copy_file_from(file: PathBuf, destination_folder: &str) {
-    let destination_folder: PathBuf = PathBuf::from(destination_folder);
+fn copy_file_from(file: PathBuf, destination_folder: &PathBuf) {
+    if !file.is_file() {
+        eprintln!("Skipping '{}': not a file", file.display());
+        return;
+    }
 
-    if file.is_file() {
-        let mut file_destination = destination_folder.to_path_buf();
-        file_destination.push(file.file_name().unwrap());
+    let mut file_destination: PathBuf = destination_folder.to_path_buf();
 
-        if let Err(e) = fs::copy(&file, &file_destination) {
-            eprintln!("Failed to copy {}: {}", file.display(), e)
-        }
+    if let Some(file_name) = file.file_name() {
+        file_destination.push(file_name);
+    } else {
+        eprintln!("Skipping '{}': path has no final component", file.display());
+        return;
+    };
+
+    if let Err(err) = fs::copy(&file, &file_destination) {
+        eprintln!(
+            "Failed to copy {} → {}: {}",
+            file.display(),
+            file_destination.display(),
+            err
+        );
     }
 }
 
-fn zip_files_in_folder(folder_path: &str) {
+pub fn zip_files_in_folder(folder_path: &str) {
     let folder = Path::new(folder_path);
     let now = Local::now();
 
@@ -67,10 +94,10 @@ fn zip_files_in_folder(folder_path: &str) {
 
     let tar_file = match File::create(&filename) {
         Ok(file) => file,
-        Err(e) => {
+        Err(err) => {
             eprintln!(
                 "Failed to create the archive file named {}: {}",
-                filename, e
+                filename, err
             );
             return;
         }
@@ -80,16 +107,20 @@ fn zip_files_in_folder(folder_path: &str) {
 
     println!("Archiving everything in the folder {}", folder_path);
 
-    if let Err(e) = archive.append_dir_all(folder_path, folder_path) {
+    if let Err(err) = archive.append_dir_all(folder_path, folder_path) {
         eprintln!(
             "Failed to append directory {} to archive: {}",
-            folder_path, e
+            folder_path, err
         );
         return;
     }
 
-    if let Err(e) = archive.finish() {
-        eprintln!("Failed to finish archive {}: {}", filename, e);
+    if let Err(err) = archive.finish() {
+        eprintln!("Failed to finish archive {}: {}", filename, err);
         return;
+    }
+
+    if let Err(err) = remove_dir_all(folder_path) {
+        println!("Failed to remove backup folder {}: {}", folder_path, err);
     }
 }
